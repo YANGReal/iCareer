959dfbfe64993992c4c140d54ebe4ce7691a9fe0
andy <yangreal@live.com>
2014-02-27 13:18:43 +0800

    init
    2014.02.27

---
+++ iCareer/NBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NBAppDelegate.h
+//  iCareer
+//
+//  Created by andy on 14-2-27.
+//  Copyright (c) 2014年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
