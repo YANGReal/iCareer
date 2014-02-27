90ad16b0b5a2e65f23e9fcd6a783c9cda2958ff3
YANGReal <yangrui@YANGRuitekiMac-mini.local>
2014-02-27 13:31:35 +0800

    test
    yangreal 2014.02.27

--- iCareer/NBAppDelegate.m
+++ iCareer/NBAppDelegate.m
@@ -16,6 +16,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    //This is create by YANGReal
     return YES;
 }
 
