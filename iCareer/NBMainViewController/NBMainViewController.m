9094bb965305e120ddcfb432058fbe3426b8b73a
Your Name <you@example.com>
2014-03-11 18:03:25 +0800

    03.11 18:03
    123456

--- iCareer/NBMainViewController/NBMainViewController.m
+++ iCareer/NBMainViewController/NBMainViewController.m
@@ -8,7 +8,7 @@
 
 #import "NBMainViewController.h"
 
-@interface NBMainViewController ()
+@interface NBMainViewController ()<UIGestureRecognizerDelegate, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet DBTileButton *chanceButton;//机会
 @property (weak, nonatomic) IBOutlet DBTileButton *messageButton;//消息
@@ -19,6 +19,9 @@
 @property (weak, nonatomic) IBOutlet UIButton *searchButton;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
+- (IBAction)closeKeboard:(id)sender;
+- (IBAction)searchButtonClicked:(UIButton *)sender;
+
 @end
 
 @implementation NBMainViewController
@@ -27,7 +30,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
@@ -35,16 +38,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    self.navigationController.interactivePopGestureRecognizer.delegate = self;
+    self.navigationController.interactivePopGestureRecognizer.enabled = YES;
     [self setupUIBarButtonItem];
     [self setupUISearchBar];
+    
+    
 }
 
 - (void)setupUIBarButtonItem
 {
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:RECT(0, 0, 100, 20)];
     [titleLabel setText:@"手机双选会"];
-    [titleLabel setFont:[UIFont boldSystemFontOfSize:20]];
+    [titleLabel setFont:[UIFont boldSystemFontOfSize:22]];
     [self.navigationItem setTitleView:titleLabel];
 
     UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -73,11 +79,31 @@
     [self.searchBar setBackgroundColor:[UIColor colorWithHexString:@"#c74842" alpha:1]];
     [self.searchBar setImage:[UIImage  createImageWithColor:[UIColor clearColor] andImageHeight:1.0f] forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
     [self.searchBar setSearchFieldBackgroundPositionAdjustment:UIOffsetMake(-10, 2)];
-    [self.searchBar setPlaceholder:@"请输入职位名称，如：会计"];
+    [self.searchBar setPlaceholder:@"请输入职位名称,如：IT"];
+
     //改变文本颜色
     UITextField *searchField = [_searchBar valueForKey:@"_searchField"];
     searchField.textColor = [UIColor whiteColor];
     [searchField setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+}
+
+#pragma mark -----各个button相应方法-----
+
+- (IBAction)closeKeboard:(id)sender
+{
+    [self.searchBar resignFirstResponder];
+}
+
+- (IBAction)searchButtonClicked:(UIButton *)sender
+{
+    if (_searchBar.text.length == 0) {
+        return;
+    }
+    else
+    {
+        DLog(@"searchText = %@", _searchBar.text);
+        self.searchBar.text = nil;
+    }
 }
 
 #pragma mark -----UIBarButtonItemAction-----
@@ -91,6 +117,26 @@
     DLog(@"this is rightItem!");
     NBLoginViewController *loginVC = [[NBLoginViewController alloc] initWithNibName:[AppUtility getNibNameFromViewController:@"NBLoginViewController"] bundle:nil];
     [self.navigationController pushViewController:loginVC animated:YES];
+}
+
+#pragma mark -----UISearchBarDelegate-----
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    DLog(@"searchText = %@", searchBar.text);
+    searchBar.text = nil;
+    [self closeKeboard:nil];
+}
+
+
+#pragma mark -----UIGestureRecognizerDelegate-----
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
+{
+    if (self.navigationController.viewControllers.count == 1)
+    {
+        return NO;
+    }
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
